1fbb368aadb9628cc68ffc5f9446166f3d02648d
junhyung001 <junhyung@gmaail.com>
2023-10-09 18:47:14 +0900

    3문제 풀이

---
+++ tear/bronze/bronze_4/exam_10089.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int arr[5];
+    int sum = 0;
+
+    for(int i=0; i<5; i++){
+        scanf("%d", &arr[i]);
+        if(arr[i]<40){
+            sum += 40;
+        }else{
+            sum += arr[i];
+        }
+    }
+    printf("%d", sum/5);
+}
